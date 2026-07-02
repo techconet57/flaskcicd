007c720488c2d929db777c51082a28100afa8c7a
dev <dev@gmail.com>
2026-07-02 18:53:04 +0545

    cicd8

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! Namastey Nepal"
+    return "Hello, World! Namastey Nepal1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
